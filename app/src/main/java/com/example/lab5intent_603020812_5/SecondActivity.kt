6218ef4c77ded9af2c1af97649d740d51abc0d3a
pattarapom085 <pattarapon.saetang@kkumail.com>
2019-09-02 16:46:06 +0700

    LAB5_SEC1_603020812-5

---
+++ app/src/main/java/com/example/lab5intent_603020812_5/SecondActivity.kt
@@ -0,0 +1,26 @@
+package com.example.lab5intent_603020812_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.lab5intent_603020812_5.R
+import com.example.lab5intent_603020812_5.Student
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        var data : Bundle? = intent.extras
+        var newStudent : Student? = data?.getParcelable<Student>("stdData")
+        idText.text = "Student ID : ${newStudent?.id}"
+        nameText.text = "Student Name : ${newStudent?.name}"
+        ageText.text = "Student Age : ${newStudent?.age}"
+    }
+
+    fun onClickClose(view: View){
+        finish()
+    }
+}
